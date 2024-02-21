400baa6d3f3e9cee4a8b9a0cc2bf2303f7a752d5
Gogita Kupatadze <kupatadze2000@outlook.com>
2024-02-21 20:25:38 +0400

    init

---
+++ ExtremeCommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ExtremeCommerce.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
